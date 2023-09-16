cedf087f2317221b9422a512c3608df6ae3eb985
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-17 02:43:19 +0300

    Массивы

---
+++ Task 3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите прогамму, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке
+// [1, 0, 1, 1, 0, 1, 0, 0]
+void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 2);
+}
+
+int[] array = new int[8];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
